1f51155d63f55503468e459bb46656d7693437fd
Pineapple Dynamics <takumawatanabe82@gmail.com>
2017-09-17 13:41:03 +0900

    Deviseの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable,:omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
 
         def self.find_for_oauth(auth)
