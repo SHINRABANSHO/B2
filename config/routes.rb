01d40db807c20b9a14c65e629068447fc9624883
Pineapple Dynamics <takumawatanabe82@gmail.com>
2017-09-17 13:51:02 +0900

    認証用リンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
